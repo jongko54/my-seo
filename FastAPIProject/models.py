2bf686a906be8e5fb3aadf891a27530864da46e3
jongko54 <datasource@kakao.com>
2026-01-05 23:35:02 +0900

    html 수정

--- FastAPIProject/models.py
+++ FastAPIProject/models.py
@@ -6,8 +6,9 @@ class Market(Base):
     __tablename__ = "market"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)   # 여기가 name 이어야 함! (title 아님)
-    content = Column(Text)                       # 여기가 content 이어야 함!
+    name = Column(String(255), nullable=False)
+    content = Column(Text)
     price = Column(Integer)
     image_url = Column(String(500))
+    url_keyword = Column(String(255), unique=True)
     create_date = Column(DateTime, default=datetime.datetime.now)
