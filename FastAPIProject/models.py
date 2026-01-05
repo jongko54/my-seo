a7efa5ff01e5376a9ca36485a2675a11ee77e7a9
jongko54 <datasource@kakao.com>
2026-01-05 23:26:20 +0900

    html 수정

--- FastAPIProject/models.py
+++ FastAPIProject/models.py
@@ -1,19 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, DateTime
 from database import Base
-
+import datetime
 
 class Market(Base):
     __tablename__ = "market"
 
     id = Column(Integer, primary_key=True, index=True)
-    url_keyword = Column(String(100), unique=True, index=True)  # 예: rose
-
-    # --- [추가된 정보] ---
-    store_name = Column(String(100), default="bnt 식물원")  # 가게 이름
-    account_number = Column(String(100))  # 계좌번호 (문자열 추천: 하이픈 포함)
-    price = Column(String(50))  # 가격 (예: "30,000원" 또는 숫자)
-    # -------------------
-
-    title = Column(String(200))
-    description = Column(String(300))
-    content = Column(Text)
+    name = Column(String(255), nullable=False)   # 여기가 name 이어야 함! (title 아님)
+    content = Column(Text)                       # 여기가 content 이어야 함!
+    price = Column(Integer)
+    image_url = Column(String(500))
+    create_date = Column(DateTime, default=datetime.datetime.now)
